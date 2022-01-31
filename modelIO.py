100812d1703a8b692c2f528631cb951b43abe782
Mustafa Barodawala <itsjustmustafa@gmail.com>
2022-01-31 14:08:13 +1100

    Basic MVC pattern template
    IO class handles files, DataFile class handles the dataframe, which uses pandas dataframes

---
+++ modelIO.py
@@ -0,0 +1,10 @@
+class IO:
+    """The file-handling class"""
+    def __init__():
+        pass
+    
+    def save():
+        raise Exception("Not implemented")
+    
+    def load():
+        raise Exception("Not implemented")
